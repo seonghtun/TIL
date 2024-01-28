bd4e1b7f37639480abf237b585b135c283075201
seonghtun <ysh970718@gmail.com>
2024-01-28 10:29:10 +0000

    add django book codes

---
+++ python/test/test/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+from burgers.models import Burger
+
+def main(request):
+    return render(request, "main.html")
+
+def burger_list(request):
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록:", burgers)
+
+    # Template 전달 dict 객체
+    context = {
+        "burgers" : burgers,
+    }
+    return render(request, "burger_list.html", context)
+
+def burger_search(request):
+    # print(request.GET)
+    keyword = request.GET.get("keyword")
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    
+    else :
+        burgers = Burger.objects.none()
+    context = {
+        "burgers" : burgers
+    }
+    print(burgers)
+
+    return render(request, "burger_search.html",context)
